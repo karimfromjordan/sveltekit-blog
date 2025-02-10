50d14183199a8908b6036dc7cdf623d54f566302
Karim Jordan <karimfromjordan@gmail.com>
2025-02-10 14:08:31 +0700

    wip

--- src/lib/content/post.ts
+++ src/lib/content/post.ts
@@ -6,7 +6,7 @@ class Post {
 	published_at?: string = undefined;
 	updated_at?: string = undefined;
 
-	content: unknown[] = [];
+	blocks = new BlockList();
 
 	constructor(params: {
 		h1: string;
@@ -15,6 +15,7 @@ class Post {
 		hero_img?: string;
 		published_at?: string;
 		updated_at?: string;
+		blocks?: BlockList;
 	}) {
 		this.h1 = params.h1;
 		this.slug = params.slug;
@@ -22,20 +23,28 @@ class Post {
 		this.hero_img = params.hero_img;
 		this.published_at = params.published_at;
 		this.updated_at = params.updated_at;
+
+		if (params.blocks) {
+			this.blocks = params.blocks;
+		}
 	}
 
 	toJSON() {}
+}
 
-	p(params: { text: string | (string | unknown)[]; class?: string }) {
-		this.content.push({ type: 'p', ...params });
+class BlockList {
+	blocks: unknown[] = [];
+
+	p(params: { inlines: InlineList; class?: string }) {
+		this.blocks.push({ type: 'block:p', ...params });
 		return this;
 	}
-	h2(params: { text: string; class?: string }) {
-		this.content.push({ type: 'h2', ...params });
+	h2(params: { inlines: InlineList; class?: string }) {
+		this.blocks.push({ type: 'block:h2', ...params });
 		return this;
 	}
-	h3(params: { text: string; class?: string }) {
-		this.content.push({ type: 'h3', ...params });
+	h3(params: { inlines: InlineList; class?: string }) {
+		this.blocks.push({ type: 'block:h3', ...params });
 		return this;
 	}
 	code(params: {
@@ -45,65 +54,78 @@ class Post {
 		highlight?: number[];
 		file_name?: string;
 	}) {
-		this.content.push({ type: 'code', ...params });
+		this.blocks.push({ type: 'block:code', ...params });
 		return this;
 	}
 	img(params: { src: string; alt: string; class?: string }) {
-		this.content.push({ type: 'img', ...params });
+		this.blocks.push({ type: 'block:img', ...params });
 		return this;
 	}
-	ol(params: { items: any; class?: string }) {
-		this.content.push({ type: 'ol', ...params });
+	ol(params: { items: InlineList[]; class?: string }) {
+		this.blocks.push({ type: 'block:ol', ...params });
 		return this;
 	}
-	ul(params: { items: any; class?: string }) {
-		this.content.push({ type: 'ul', ...params });
+	ul(params: { items: InlineList[]; class?: string }) {
+		this.blocks.push({ type: 'block:ul', ...params });
 		return this;
 	}
-	quote(params: { text: string; author?: string; href?: string }) {
+	quote(params: { inlines: InlineList; author?: string; href?: string }) {
 		return this;
 	}
-	table(params: { header: string[]; data: Array<string[]> }) {
+	table(params: { header: InlineList[]; data: Array<InlineList[]> }) {
 		return this;
 	}
-	note(params: { text: string }) {
-		this.content.push({ type: 'note', ...params });
+	note(params: { blocks: BlockList }) {
+		this.blocks.push({ type: 'block:note', ...params });
 		return this;
 	}
-	tip(params: { text: string }) {
-		this.content.push({ type: 'tip', ...params });
+	tip(params: { blocks: BlockList }) {
+		this.blocks.push({ type: 'block:tip', ...params });
 		return this;
 	}
-	important(params: { text: string }) {
-		this.content.push({ type: 'important', ...params });
+	important(params: { blocks: BlockList }) {
+		this.blocks.push({ type: 'block:important', ...params });
 		return this;
 	}
-	warning(params: { text: string }) {
-		this.content.push({ type: 'warning', ...params });
+	warning(params: { blocks: BlockList }) {
+		this.blocks.push({ type: 'block:warning', ...params });
 		return this;
 	}
-	caution(params: { text: string }) {
-		this.content.push({ type: 'caution', ...params });
+	caution(params: { blocks: BlockList }) {
+		this.blocks.push({ type: 'block:caution', ...params });
 		return this;
 	}
 }
 
-function link(text: string, href: `https://${string}`, opts?: { class?: string }) {
-	return { type: 'inline:link', text, href, ...opts };
+class InlineList {
+	inlines = [];
+
+	text(text: string, opts?: { class?: string }) {
+		this.inlines.push(text);
+		return this;
+	}
+	link(text: string, href: `https://${string}`, opts?: { class?: string }) {
+		this.inlines.push({ type: 'inline:link', text, href, ...opts });
+		return this;
+	}
+	strong(text: string, opts?: { class?: string }) {
+		this.inlines.push({ type: 'inline:strong', text, ...opts });
+		return this;
+	}
 }
 
-function strong(text: string, opts?: { class?: string }) {
-	return { type: 'inline:strong', text, ...opts };
-}
-
-export { Post, link, strong };
-
-const post = new Post({ h1: 'First post' })
-	.p({
-		class: 'text-white',
-		text: ['This is a paragraph', link('Google', 'https://google.com')]
-	})
-	.code({
-		lang: 'js',
-		code: 'console.log("Hello")'
-	});
+const post = new Post({
+	h1: 'First post',
+	slug: 'first-post',
+	blocks: new BlockList()
+		.p({
+			class: 'text-white',
+			inlines: new InlineList()
+				.text('Svelte is a major JavaScript framework with a great community. Check it our at')
+				.link('Svelte', 'https://svelte.dev')
+		})
+		.code({
+			lang: 'js',
+			code: 'console.log("Hello")'
+		})
+});
